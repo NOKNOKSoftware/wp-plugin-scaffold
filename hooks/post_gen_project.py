d87d20f734e47114705207a67337403667fab75e
Cody Rees <cody@noknokstudios.com>
2021-05-11 16:46:40 +1000

    Fixed path error in python script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,10 +4,7 @@ import shutil
 {% for name, settings in cookiecutter.sass_projects|dictsort %}
 
 path = "./assets/sass/{{ name }}/"
-
-if os.path.exists(path):
-    shutil.rmtree(path)
-
+shutil.rmtree( path, ignore_errors=True)
 os.mkdir(path)
 
 file = "./assets/sass/{{ name }}/{{ name }}.scss"
@@ -19,17 +16,13 @@ open( file, 'a' ).close()
 {% for name, settings in cookiecutter.js_projects|dictsort %}
 
 path = "./assets/js/{{ name }}/"
-
-if os.path.exists(path):
-    shutil.rmtree(path)
-
+shutil.rmtree( path, ignore_errors=True)
 os.mkdir(path)
 
 file = "./assets/js/{{ name }}/{{ name }}.js"
 open( file, 'a' ).close()
 
 {% endfor %}
-
 
 os.system( "git init" )
 os.system( "npm install" )
